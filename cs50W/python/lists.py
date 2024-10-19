9a4a04230714aea16706ac8058716adf4b49e97e
Maaxboon <maaxwelokoth@gmail.com>
2024-10-19 19:57:52 +0300

    Adds lists.py

---
+++ cs50W/python/lists.py
@@ -0,0 +1,15 @@
+# Define a list of names
+names = ['Max', 'Phil', 'James', 'Bill']
+
+# print(names) # prints all names
+# print(names[0]) # prints Max
+
+names.append('Joseph') # Adds Joseph to the end of the list
+
+# print(names) # prints all names including Joseph
+
+# print(names) # prints all names including Joseph
+
+names.sort() # sorts the names 
+
+print(names) # prints names in alphabetical order
