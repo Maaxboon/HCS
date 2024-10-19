07887f55a2d7ce809c85fb91e7bdb7e35b8dd60f
Maaxboon <maaxwelokoth@gmail.com>
2024-10-19 19:48:56 +0300

    Adds sequences.py

---
+++ cs50W/python/sequences.py
@@ -0,0 +1,19 @@
+# Indexing
+# name = "Henry"
+
+# print(name[0]) # prints H
+
+
+# List
+# names = ['Maax', 'Phil', 'James', 'Bill']
+
+# print(names) # prints all list items
+
+# print(names[0]) # prints Maax
+
+# Tuple
+
+# coordinateX = 10.0
+# coordinateY = 20.0
+# coordinate = (10.0, 20.0)
+
