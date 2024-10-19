b41d100c037120852f795dd4c10214b01fb5e27e
Maaxboon <maaxwelokoth@gmail.com>
2024-10-19 20:32:08 +0300

    Adds disctionaries.py

---
+++ cs50W/python/dictionaries.py
@@ -0,0 +1,8 @@
+residences = {"Phil": "Zindiro", "Maax": "Kwanaizira", "Wafula": "Kibagabaga", "James": "Soul"}
+# print(residences) # Prints all names and residences
+
+# print(residences["Phil"]) # Prints Zindiro
+
+residences["James"] = "Kizingo"
+
+print(residences["James"]) # Prints Kizingo
