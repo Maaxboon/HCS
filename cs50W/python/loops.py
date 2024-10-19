6679b9867bd5a0c2f2ec14f71ce6c1e0a69c768f
Maaxboon <maaxwelokoth@gmail.com>
2024-10-19 20:21:55 +0300

    Adds loops.py

---
+++ cs50W/python/loops.py
@@ -0,0 +1,21 @@
+# for i in [0, 1, 2, 3, 4, 5]:
+
+# Perform same function as above
+for i in range(6):
+    print(i)
+
+
+names = ['Kenya', 'Uganda', 'Rwanda']
+
+for name in names:
+    print(name)
+
+career = 'developer'
+
+for character in career:
+    print(i)
+
+phones = ('iPhone', 'Huawei', 'Nokia', 'Infinix', 'Samsung')
+
+for simu in phones:
+    print(simu)
