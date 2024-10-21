5c0bb6d9e9d656dc8dfd5a41f98dd03a5b3792a2
Maaxboon <maaxwelokoth@gmail.com>
2024-10-21 08:25:45 +0300

    Adds Maaax view function

--- cs50W/Django/lecture3/hello/views.py
+++ cs50W/Django/lecture3/hello/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    return HttpResponse("Hello, World!")
+
+def Maaax(request):
     return HttpResponse("Hello, Maaax!")
