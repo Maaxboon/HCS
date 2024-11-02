c48d31558f5867b3eb7b87335f139abbaa9dc5b5
Maaxboon <maaxwelokoth@gmail.com>
2024-11-02 20:08:02 +0300

    Changes the routes

--- cs50W/Django/lecture3/hello/urls.py
+++ cs50W/Django/lecture3/hello/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    
+    path("<str:name>" , views.greet, name="greet")
     path("Maaax", views.Maaax, name="Maaax"),
     path("Fanice", views.Fanice, name="Fanice")
 ]
